bd136aec1947067b442d8d225777d7204fa5f949
Karén <info@karenishe.com>
2017-10-13 00:00:00 +0200

    Enable unicode characters

--- functions/webhook/utils/checkSignature.js
+++ functions/webhook/utils/checkSignature.js
@@ -1,14 +1,9 @@
 'use strict';
 
 import crypto from 'crypto';
-import jsesc from 'jsesc';
 
 export function checkSignature(signature, payload) {
-  payload = jsesc(payload, {
-    escapeEverything: false,
-    lowercaseHex: true,
-    json: true,
-  });
+  payload = payload.replace(/\//g, '\/').replace(/</g, '\\u003c').replace(/%/g, '\\u0025').replace(/@/g, '\\u0040');
   const signatureHash = crypto.createHmac('sha1', process.env.APP_SECRET).update(payload).digest('hex');
   return signature.substr(5) === signatureHash;
 };
