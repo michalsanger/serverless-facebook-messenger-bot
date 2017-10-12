2657fd32317aea28589290539a6c3a2dc86c1e45
Karén <info@karenishe.com>
2017-10-13 00:03:10 +0200

    Update checkSignature.js

--- functions/webhook/utils/checkSignature.js
+++ functions/webhook/utils/checkSignature.js
@@ -3,7 +3,7 @@
 import crypto from 'crypto';
 
 export function checkSignature(signature, payload) {
-  payload = payload.replace(/\//g, '\/').replace(/</g, '\\u003c').replace(/%/g, '\\u0025').replace(/@/g, '\\u0040');
+  payload = JSON.stringify(payload).replace(/\//g, '\/').replace(/</g, '\\u003c').replace(/%/g, '\\u0025').replace(/@/g, '\\u0040');
   const signatureHash = crypto.createHmac('sha1', process.env.APP_SECRET).update(payload).digest('hex');
   return signature.substr(5) === signatureHash;
 };
